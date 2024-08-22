cc107384fcbbf10a869ccbe2139bc3ef4e38e453
尹韶璞 <22371277>
2024-08-22 15:47:50 +0800

    或许gateway路由配好了？

--- backend/teach-service/src/main/java/com/example/teachapi/controller/UploadController.java
+++ backend/teach-service/src/main/java/com/example/teachapi/controller/UploadController.java
@@ -14,7 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("upload")
+@RequestMapping("api/upload")
 public class UploadController {
 
     @Autowired
