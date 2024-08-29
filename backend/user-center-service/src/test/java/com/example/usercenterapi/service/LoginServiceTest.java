df309f3c94015a78dcd90bd0093f811e659e346d
尹韶璞 <22371277>
2024-08-29 15:32:26 +0800

    1

--- backend/user-center-service/src/test/java/com/example/usercenterapi/service/LoginServiceTest.java
+++ backend/user-center-service/src/test/java/com/example/usercenterapi/service/LoginServiceTest.java
@@ -1,4 +1,58 @@
+package com.example.usercenterapi.service;
+
+import com.example.usercenterapi.vo.Result;
+import com.example.usercenterapi.vo.params.LoginParam;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
 class LoginServiceTest {
-  
+
+    @Autowired
+    private LoginService loginService;
+
+    @Test
+    void loginPositive() {
+        LoginParam loginParam=new LoginParam();
+        loginParam.setAccount("admin");
+        loginParam.setPassword("admin");
+        Result result=loginService.login(loginParam);
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getData());
+    }
+
+    @Test
+    void loginNegative() {
+        LoginParam loginParam=new LoginParam();
+        loginParam.setAccount("");
+        loginParam.setPassword("");
+        Result result=loginService.login(loginParam);
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getData());
+    }
+
+    @Test
+    void checkToken() {
+    }
+
+    @Test
+    void logout() {
+    }
+
+    @Test
+    void registerPositive() {
+        LoginParam loginParam=new LoginParam();
+        loginParam.setAccount("junittest");
+        loginParam.setPassword("junittest");
+        loginParam.setNickname("单元测试");
+        Result result=loginService.register(loginParam);
+        Assertions.assertTrue(result.isSuccess());
+        Assertions.assertNotNull(result.getData());
+    }
 }
---
+++ backend/user-center-service/src/test/java/com/example/usercenterapi/service/SysUserServiceTest.java
@@ -0,0 +1,68 @@
+package com.example.usercenterapi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SysUserServiceTest {
+
+    @Test
+    void findUser() {
+    }
+
+    @Test
+    void findUserByToken() {
+    }
+
+    @Test
+    void findUserByAccount() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void updateUser() {
+    }
+
+    @Test
+    void getInfoById() {
+    }
+
+    @Test
+    void updateAvatar() {
+    }
+
+    @Test
+    void selectById() {
+    }
+
+    @Test
+    void selectPage() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void updateById() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void selectBatchIds() {
+    }
+
+    @Test
+    void deleteBatchIds() {
+    }
+
+    @Test
+    void update() {
+    }
+}
