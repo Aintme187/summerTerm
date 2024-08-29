f6a7162ff2c8cbfd2347bbabb2ff190970dc04aa
toioo <982903879@qq.com>
2024-08-29 14:25:32 +0800

    blog-service增加test包，下有测试用例

---
+++ backend/blog-service/src/test/java/com/example/blogapi/service/TagServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.blogapi.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TagServiceTest {
+
+    @Test
+    void findTagsByArticleId() {
+    }
+
+    @Test
+    void hot() {
+    }
+
+    @Test
+    void findAllTags() {
+    }
+
+    @Test
+    void findAllDetail() {
+    }
+
+    @Test
+    void findDetailById() {
+    }
+
+    @Test
+    void create() {
+    }
+}
